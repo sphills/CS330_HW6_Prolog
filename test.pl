76c6fcc15b46c3c8adad0efc786422064f37c91b
Sage Phillips <sage242@hotmail.com>
2024-11-18 20:48:23 -0600

    Implemented count and raise.

--- test.pl
+++ test.pl
@@ -51,3 +51,23 @@ compete(X, Y) :-
 	X \= Y,
 	commonPrey(X, Y, Z),
 	\+inTheFoodChain(Y, X).
+
+count([], 0). % Base case: an empty list has 0 elements.
+count([H|T], Count) :- % Recursive case: if the head is a list, count its elements recursively.
+    is_list(H),
+    count(H, CountH),
+    count(T, CountT),
+    Count is CountH + CountT.
+count([H|T], Count) :- % Recursive case: if the head is not a list, count it as 1.
+    \+ is_list(H),
+    count(T, CountT),
+    Count is 1 + CountT.
+
+raise([], []). % Base case: empty list means empty output list
+raise([H|T], [NewH|L2]) :- % Recursive case: process each number in the list.
+    NewH is round(H + (H * (201 / 10000))),            % Apply the percentage increase to H.
+    raise(T, L2).                        % Recursively process the tail.
+
+insert(E, L1, L2) :- !.
+
+delete(E, L1, L2) :- !.
